ec0b711cb38201ed723c06d32e522947ebd0191b
uhyot <v.newton.apple@gmail.com>
2020-12-16 22:10:13 +0900

    1st commit for utils

---
+++ utils/libft/ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkitagaw <tkitagaw@student.42.jp>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 10:56:13 by tkitagaw          #+#    #+#             */
+/*   Updated: 2020/12/13 12:19:47 by teppei           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_atoi(char *str)
+{
+	int				i;
+	int				sign;
+	int				len;
+	unsigned int	nbr;
+
+	i = 0;
+	sign = 1;
+	len = 0;
+	nbr = 0;
+	while (str[i] == ' ' || (8 < str[i] && str[i] < 14))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1 * sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + (str[i] - '0');
+		i++;
+	}
+	return (nbr * sign);
+}
