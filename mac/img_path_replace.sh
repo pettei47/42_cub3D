27d29dbd0a7e3fe3a0b1018b8a413caf35d5b2f9
pettei47 <kitagawa.teppei.33c@gmail.com>
2020-12-22 21:40:14 +0900

    不要なファイルを削除(mac)

--- mac/img_path_replace.sh
+++
@@ -1,7 +0,0 @@
-for file in `\find . -name '*.cub'`; do
-    sed -i -e 's/\.\/path_to_the_north_texture/\.\/images\/kita.xpm/g' $file
-    sed -i -e 's/\.\/path_to_the_south_texture/\.\/images\/minami.xpm/g' $file
-    sed -i -e 's/\.\/path_to_the_east_texture/\.\/images\/higashi.xpm/g' $file
-    sed -i -e 's/\.\/path_to_the_west_texture/\.\/images\/nishi.xpm/g' $file
-    sed -i -e 's/\.\/path_to_the_sprite_texture/\.\/images\/eagle.xpm/g' $file
-done
