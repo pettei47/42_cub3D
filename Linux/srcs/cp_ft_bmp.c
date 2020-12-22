b913c0decd5bee94b0b2dab461a1963a689c5662
Sho Uchida <suchida@c1r1s6.42tokyo.jp>
2020-12-22 21:20:22 +0900

    fix for norm

--- Linux/srcs/cp_ft_bmp.c
+++ Linux/srcs/cp_ft_bmp.c
@@ -6,7 +6,7 @@
 /*   By: teppei <teppei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 20:34:07 by teppei            #+#    #+#             */
-/*   Updated: 2020/12/15 21:21:19 by teppei           ###   ########.fr       */
+/*   Updated: 2020/12/22 21:19:53 by suchida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,8 @@ static void	build_pixel_data(t_all *a, char *pixel_data, int x_pad)
 	{
 		j = 0;
 		while (j < a->e->win_x * 4)
-			pixel_data[i * (a->e->win_x + x_pad) * 4 + j++] \
-				= a->img.addr[k++];
+			pixel_data[i * (a->e->win_x + x_pad) * 4 + j++] = a->img.addr[k++];
 		p = 0;
-//		while (p < x_pad)
-//			pixel_data[i * a->e->win_x * 4 + j + p++] = 0;
 		i--;
 	}
 }
@@ -78,7 +75,6 @@ void		make_bmp(t_all *a)
 	fd = open("capture_cub3D.bmp", O_CREAT | O_WRONLY, 0644);
 	if (fd == -1)
 		error_free_a("cannot open file bmp", a);
-	//x_pad = a->e->win_x % 64;
 	x_pad = a->e->win_x;
 	n_pixel = (a->e->win_x + x_pad) * a->e->win_y;
 	if (!(pixel_data = (char *)malloc(sizeof(char) * n_pixel * 4)))
